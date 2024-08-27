10a57545a6161de15f814e8a955de1a4fe42107c
dolph <dolphinclan@outlook.com>
2024-08-27 18:00:49 +0800

    commit

---
+++ assignment/unit1/1.1.1.java
@@ -0,0 +1,10 @@
+package unit1;
+
+class main {
+    public static boolean main(char a, char b) {
+        return a == b;
+    }
+    public static void main(String[] args) {
+        System.out.println(main('a', 'a'));
+    }
+}
