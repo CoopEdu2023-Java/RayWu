a230e041d0389160daee19919d23b6497500191a
dolph <dolphinclan@outlook.com>
2024-08-28 14:11:03 +0800

    commit

---
+++ assignment/unit2/1.1.1.java
@@ -0,0 +1,20 @@
+class Demo {
+    private String name = "person";
+    private int age = 20;
+    private String country = "china";
+
+    public String getName() {
+        return this.name;
+    }
+    public Integer getAge() {
+        return this.age;
+    }
+    public String getCountry() {
+        return this.country;
+    }
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
