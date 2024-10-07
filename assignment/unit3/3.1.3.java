1c7c2cbcc18e2d5b3bbf8875fb382930bccb43ca
RayWu-1 <raywu@moonshotacademy.cn>
2024-10-07 11:48:05 +0800

    Delete assignment directory

--- assignment/unit3/3.1.3.java
+++
@@ -1,9 +0,0 @@
-class ArrayList {
-    private int[] data;
-    private int size;
-
-    public ArrayList(int capacity) {
-        this.data = (int[]) new int[capacity];
-        this.size = 0;
-    }
-}
